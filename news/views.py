f6fe543d65f987afed15634d9dd90fafa3f103b6
AlexandreFelippe <alemfelippe@gmail.com>
2024-05-06 17:17:57 -0300

    Corrige requisito 07

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import News, Category
+from .models import News, Category, User
 from .forms import CreateCategoriesForm, CreateNewsForm
 from rest_framework import viewsets
 from .serializers import CategorySerializer
@@ -28,15 +28,14 @@ def categories_form(request):
 
 def news_form(request):
     form = CreateNewsForm()
+    users = User.objects.all()
+    categories = Category.objects.all()
     if request.method == "POST":
         form = CreateNewsForm(request.POST, request.FILES)
         if form.is_valid():
-            categories = form.cleaned_data.pop("categories").split(",")
-            news = News.objects.create(**form.cleaned_data)
-            for category in categories:
-                news.categories.add(Category.objects.get(name=category))
+            News.objects.create(**form.cleaned_data)
             return redirect("home-page")
-    context = {"form": form}
+    context = {"form": form, "users": users, "categories": categories}
     return render(request, "news_form.html", context)
 
 
