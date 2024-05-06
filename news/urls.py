6caa82d0e07aef6240a207a3cd7c480133620ec9
AlexandreFelippe <alemfelippe@gmail.com>
2024-05-06 14:55:52 -0300

    Resolve requisito 06

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, details
+from .views import index, details, categories_form
 
 urlpatterns = [
     path("", index, name="home-page"),
-    path("/<int:id>", details, name="news-details-page")
+    path("/<int:id>", details, name="news-details-page"),
+    path("categories", categories_form, name="categories-form")
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import News
+from django.shortcuts import render, redirect
+from .models import News, Category
+from .forms import CreateCategoriesForm
 
 
 def index(request):
@@ -10,3 +11,14 @@ def index(request):
 def details(request, id):
     news = News.objects.get(id=id)
     return render(request, "news_details.html", {"news": news})
+
+
+def categories_form(request):
+    form = CreateCategoriesForm()
+    if request.method == "POST":
+        form = CreateCategoriesForm(request.POST)
+        if form.is_valid():
+            Category.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+    context = {"form": form}
+    return render(request, "categories_form.html", context)
