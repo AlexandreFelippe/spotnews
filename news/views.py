4855ae46e12e6f2cf3287c41d93c574fa953f505
AlexandreFelippe <alemfelippe@gmail.com>
2024-05-06 09:57:50 -0300

    Resolve requisito 05

--- news/views.py
+++ news/views.py
@@ -5,3 +5,8 @@ from .models import News
 def index(request):
     news = News.objects.all()
     return render(request, "home.html", {"news": news})
+
+
+def details(request, id):
+    news = News.objects.get(id=id)
+    return render(request, "news_details.html", {"news": news})
