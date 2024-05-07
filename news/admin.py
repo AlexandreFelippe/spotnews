743e8d96c0b3c7f278c3b792b7ec6f3837769668
AlexandreFelippe <alemfelippe@gmail.com>
2024-05-07 16:04:46 -0300

    add superuse

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import News, Category, User
 
-# Register your models here.
+admin.site.register(News)
+admin.site.register(Category)
+admin.site.register(User)
